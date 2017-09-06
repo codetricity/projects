0e6b86493be8e65527b0ea077189ea461a35e456
Caitlyn <craigoda+caitlynbakery@gmail.com>
2017-09-06 07:29:57 -0700

    restaurants

--- restaurant.py
+++ restaurant.py
@@ -1,9 +1,36 @@
 import random
-meal = input("What meal do you want? Breakfast, lunch, or dinner? ")
-if meal == "lunch":
-    print("You are having lunch")
 
-restaurants = [
+
+breakfastlist = [
+    "Avenue Cafe",
+    "The Sand Bar",
+    "Zelda's",
+    "Cook House",
+    "Sunrise Cafe"
+]
+
+lunchlist = [
+    "Sushi Garden",
+    "Erik's Deli Cafe",
+    "Betty's Burger",
+    "Chipotle",
+    "Togos",
+    "Dharma's",
+    "Thai Basil"
+]
+
+snacklist = [
+    "iCrave",
+    "Yogurtland",
+    "Penny Creamery",
+    "Baskin Robbins",
+    "Gayle's Bakery",
+    "Jamba Juice",
+    "Verve",
+    "Mr. Toot's Coffeehouse",
+    "Off the Block"
+]
+fulllist = [
     "Sushi Garden",
     "Asian Express",
     "Mayflower",
@@ -20,8 +47,16 @@ restaurants = [
     "East Side Eatery"
 ]
 
+meal = input("What meal do you want? Breakfast, lunch, snack, or dinner? ")
+if meal == "breakfast":
+    restaurants = breakfastlist
+elif meal == "lunch":
+    restaurants = lunchlist
+elif meal == "snack":
+    restaurants = snacklist
+
 restaurantnum = len(restaurants)
 restaurantran = random.randrange(0, restaurantnum)
 restaurantstr = str(restaurantnum)
-print("There are " + restaurantstr + " restaurants in the Capitola list")
+print("There are " + restaurantstr + " restaurants in the " + meal + " list")
 print("How about " + restaurants[restaurantran] + "?")
